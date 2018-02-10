6b0c7d5caa1183b5676800bbc696ec9b6ca615ee
May Chen <maychen@Mays-MacBook-Pro.local>
2018-02-10 01:51:10 -1000

    completed final boss

--- exercises.js
+++ exercises.js
@@ -362,7 +362,33 @@ console.log("doughnutBought: ", doughnutBought); //2
 /*Create a function name dailySpecials which takes in a parameter: `special`.
 Inside the function, create a switch statement that will check the daily specials of your favorite restaurant (or make up your own daily specials for each day of the week.*/
 
+function dailySpecials(special){
 
+	switch(special){
+
+		case "Monday":
+			return "pizza";
+			break;
+		case "Tuesday":
+			return "burgers";
+			break;
+		case "Wednesday":
+			return "noodles";
+			break;
+		case "Thursday":
+			return "fries";
+			break;
+		case "Friday":
+			return "ice cream";
+			break;
+		default:
+			return "poke";
+			break;
+	}
+}
+
+var todaySpecial = dailySpecials("Friday");
+console.log("Today's special is: ", todaySpecial);
 
 /*
 For loops - A for loop checks a condition a specific number of times and allows us to execute a code block and evaluate a condition to determine if our loop should run again.
