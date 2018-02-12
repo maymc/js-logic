0c65c62ff25a444f446d647aecfa4f51ece3ae61
May Chen <maychen@Mays-MacBook-Pro.local>
2018-02-11 23:14:49 -1000

    completed ex16

--- exercises.js
+++ exercises.js
@@ -437,6 +437,10 @@ for(var i =0; i<players.length; i++){
 */
   var myFavFoods = ["lemon bar", "carrot cake", "nachos", "bacon cheeseburger", "ramen", "sweet potato fries", "chimichanga"];
 
+for(var i=0; i<myFavFoods.length; i++){
+	console.log(myFavFoods[i]);
+}
+
 
 /*
  * #17
